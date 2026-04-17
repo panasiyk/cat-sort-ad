df7a155cba7608988e38b49030241f0ace22a01e
AndriiPanasiuk <Andrii.Panasiuk@onseo.biz>
2026-04-17 14:19:13 +0300

    update folder name

--- src/core/AssetLoader.ts
+++ src/core/AssetLoader.ts
@@ -1,76 +1,41 @@
 import {Assets, type Texture} from 'pixi.js';
 import {CatColor} from '../types';
 
-import bgUrl from '/back.png';
-import overlayUrl from '/overlay.png';
-import shelvesUrl from '/shelves.png';
-import buttonUrl from '/Button.png';
-import ctaUrl from '/Call to action.png';
-import likeUrl from '/like.png';
-
-import shelfWhiteUrl from '/shelves/shelve_white.png';
-import shelfYellowUrl from '/shelves/shelve_yellow.png';
-import shelfPinkUrl from '/shelves/shelve_pink.png';
-import shelfBlueUrl from '/shelves/shelve_blue.png';
-import shelfGreenUrl from '/shelves/shelve_green.png';
-import shelfOrangeUrl from '/shelves/shelve_orange.png';
-
-import catIdleWhiteUrl from '/cats_idle/White_cat_idle.png';
-import catIdleYellowUrl from '/cats_idle/Yellow_cat_idle.png';
-import catIdlePinkUrl from '/cats_idle/Pink_cat_idle.png';
-import catIdleBlueUrl from '/cats_idle/Blue_cat_idle.png';
-import catIdleGreenUrl from '/cats_idle/Green_cat_idle.png';
-import catIdleOrangeUrl from '/cats_idle/Orange_Cat_idle.png';
-
-import catSelectWhiteUrl from '/cats_select/White_cat_select.png';
-import catSelectYellowUrl from '/cats_select/Yellow_cat_select.png';
-import catSelectPinkUrl from '/cats_select/Pink_Cat_select.png';
-import catSelectBlueUrl from '/cats_select/Blue_cat_select.png';
-import catSelectGreenUrl from '/cats_select/Green_Cat_select.png';
-import catSelectOrangeUrl from '/cats_select/Orange_cat_select.png';
-
-import catSleepWhiteUrl from '/cats_sllep/White_Cat_sleep.png';
-import catSleepYellowUrl from '/cats_sllep/Yellow_cat_sleep.png';
-import catSleepPinkUrl from '/cats_sllep/Pink_Cat_sleep.png';
-import catSleepBlueUrl from '/cats_sllep/Blue_cat_sleep.png';
-import catSleepGreenUrl from '/cats_sllep/Green_Cat_sleep.png';
-import catSleepOrangeUrl from '/cats_sllep/Orange_Cat_sleep.png';
-
 const MANIFEST: Record<string, string> = {
-    bg: bgUrl,
-    overlay: overlayUrl,
-    shelves: shelvesUrl,
-    button: buttonUrl,
-    cta: ctaUrl,
-    like: likeUrl,
+    bg: '/back.png',
+    overlay: '/overlay.png',
+    shelves: '/shelves.png',
+    button: '/Button.png',
+    cta: '/Call to action.png',
+    like: '/like.png',
 
-    shelf_white: shelfWhiteUrl,
-    shelf_yellow: shelfYellowUrl,
-    shelf_pink: shelfPinkUrl,
-    shelf_blue: shelfBlueUrl,
-    shelf_green: shelfGreenUrl,
-    shelf_orange: shelfOrangeUrl,
+    shelf_white: '/shelves/shelve_white.png',
+    shelf_yellow: '/shelves/shelve_yellow.png',
+    shelf_pink: '/shelves/shelve_pink.png',
+    shelf_blue: '/shelves/shelve_blue.png',
+    shelf_green: '/shelves/shelve_green.png',
+    shelf_orange: '/shelves/shelve_orange.png',
 
-    cat_idle_white: catIdleWhiteUrl,
-    cat_idle_yellow: catIdleYellowUrl,
-    cat_idle_pink: catIdlePinkUrl,
-    cat_idle_blue: catIdleBlueUrl,
-    cat_idle_green: catIdleGreenUrl,
-    cat_idle_orange: catIdleOrangeUrl,
+    cat_idle_white: '/cats_idle/White_cat_idle.png',
+    cat_idle_yellow: '/cats_idle/Yellow_cat_idle.png',
+    cat_idle_pink: '/cats_idle/Pink_cat_idle.png',
+    cat_idle_blue: '/cats_idle/Blue_cat_idle.png',
+    cat_idle_green: '/cats_idle/Green_cat_idle.png',
+    cat_idle_orange: '/cats_idle/Orange_Cat_idle.png',
 
-    cat_select_white: catSelectWhiteUrl,
-    cat_select_yellow: catSelectYellowUrl,
-    cat_select_pink: catSelectPinkUrl,
-    cat_select_blue: catSelectBlueUrl,
-    cat_select_green: catSelectGreenUrl,
-    cat_select_orange: catSelectOrangeUrl,
+    cat_select_white: '/cats_select/White_cat_select.png',
+    cat_select_yellow: '/cats_select/Yellow_cat_select.png',
+    cat_select_pink: '/cats_select/Pink_Cat_select.png',
+    cat_select_blue: '/cats_select/Blue_cat_select.png',
+    cat_select_green: '/cats_select/Green_Cat_select.png',
+    cat_select_orange: '/cats_select/Orange_cat_select.png',
 
-    cat_sleep_white: catSleepWhiteUrl,
-    cat_sleep_yellow: catSleepYellowUrl,
-    cat_sleep_pink: catSleepPinkUrl,
-    cat_sleep_blue: catSleepBlueUrl,
-    cat_sleep_green: catSleepGreenUrl,
-    cat_sleep_orange: catSleepOrangeUrl,
+    cat_sleep_white: '/cats_sllep/White_Cat_sleep.png',
+    cat_sleep_yellow: '/cats_sllep/Yellow_cat_sleep.png',
+    cat_sleep_pink: '/cats_sllep/Pink_Cat_sleep.png',
+    cat_sleep_blue: '/cats_sllep/Blue_cat_sleep.png',
+    cat_sleep_green: '/cats_sllep/Green_Cat_sleep.png',
+    cat_sleep_orange: '/cats_sllep/Orange_Cat_sleep.png',
 };
 
 
